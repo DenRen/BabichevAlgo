f20178e41e88c7e833cab59b79aadfb9e14f7131
DenRen <denrenruslan@gmail.com>
2022-04-03 20:33:10 +0300

    Update template

---
+++ H5/H5.3-server/solve.hpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <cstdlib>
+#include <cassert>
+#include <map>
+#include <queue>
+#include <set>
+#include <iomanip>
+#include <stack>
+#include <fstream>
+
+// g++ -DHOST -std=c++17 main.cpp
+
+// #define NDEBUG
+// #define HOST
+
+#ifdef HOST 
+    #include "../../libs/print_lib.hpp"
+    #define DUMP(obj) std::cout << #obj ": " << obj << "\n"
+#else
+    #define DUMP(obj)
+#endif
+
+int
+solve (std::size_t N, std::size_t M) {
+    for 
+}
--- template/main.cpp
+++ template/main.cpp
@@ -1,5 +1,5 @@
 #include "solve.hpp"
 
 int main () {
-
+    std::ios_base::sync_with_stdio (false);
 }
