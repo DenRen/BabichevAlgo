58f2f181c7a14fe531523d40ffb983830d16197e
DenRen <denrenruslan@gmail.com>
2022-04-17 19:58:49 +0300

    Solved H6.5

--- H6/H6.5-phone/main.cpp
+++ H6/H6.5-phone/main.cpp
@@ -1,11 +1,28 @@
-#include "solve.hpp"
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 std::size_t
+msb_up (int n) {
+    int res = 0;
+    while (n) {
+        n >>= 1;
+        ++res;
+    }
+
+    return res;
+}
+
+std::size_t
 solve (std::size_t n, std::size_t k) {
+    if (k > 31) {
+        k = 31;
+    }
+
+    if ((1 << k) > n) {
+        k = msb_up (n);
+    }
+
     std::vector <std::size_t> S_prev (n + 1), S (n + 1);
 
     // S[k][i] - num of S in i + 1 floors, k + 1 phones
@@ -15,30 +32,30 @@ solve (std::size_t n, std::size_t k) {
     for (int num_floor = 1; num_floor <= n; ++num_floor) {
         S_prev[num_floor] = num_floor - 1;
     }
-    DUMP (S_prev);
+    
     auto* S_prev_ptr = &S_prev, *S_ptr = &S;
 
     for (int cur_k = 2; cur_k <= k; ++cur_k) {
         auto& cur_arr = *S_ptr;
         auto& prev_arr = *S_prev_ptr;
-
+        
         cur_arr[0] = 0;
         cur_arr[1] = 0;
+
+        std::size_t min_s = 1, i_floor = 1;
         for (int num_floor = 2; num_floor <= n; ++num_floor) {
-            std::size_t min_s = -1;
-            for (int i = 1; i <= num_floor; ++i) {
-                std::size_t s = 1 + std::max (
-                    cur_arr[num_floor - i], prev_arr[i]
-                );
-                
-                if (s < min_s) {
-                    min_s = s;
-                }
+            std::size_t s_down       = 1 + std::max (cur_arr[num_floor - i_floor],
+                                                     prev_arr[i_floor]);
+            std::size_t s_down_right = 1 + std::max (cur_arr[num_floor - i_floor - 1],
+                                                     prev_arr[i_floor + 1]); 
+            if (s_down_right < s_down) {
+                ++i_floor;
+                cur_arr[num_floor] = s_down_right;
+            } else {
+                cur_arr[num_floor] = s_down;
             }
-            
-            cur_arr[num_floor] = min_s;
-        }
-        DUMP (S);
+       }
+        
         std::swap (S_ptr, S_prev_ptr);
     }
 
@@ -47,6 +64,8 @@ solve (std::size_t n, std::size_t k) {
 
 int main () {
     // 1 <= 100'000 -> (1 << 17) > 100'000
+
+    std::ios_base::sync_with_stdio (false);
 
     std::size_t n = 0, k = 0;
     std::cin >> n >> k;
