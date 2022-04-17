ad3893940d871c8ff3bc8e851afced33bccdcc6f
DenRen <denrenruslan@gmail.com>
2022-04-17 18:31:03 +0300

    Соптимизировал решение

--- H6/H6.5-phone/main.cpp
+++ H6/H6.5-phone/main.cpp
@@ -6,28 +6,29 @@
 
 std::size_t
 solve (std::size_t n, std::size_t k) {
-    std::vector <std::vector <std::size_t>> S (k);
-    for (auto& s : S) {
-        s.resize (n + 1);
-        std::fill (s.begin (), s.end (), -1);
-    }
+    std::vector <std::size_t> S_prev (n + 1), S (n + 1);
 
     // S[k][i] - num of S in i + 1 floors, k + 1 phones
     // Fill S by k == 1
     // S[0][n] = n - 1 => S[0] = 0, 0, 1, 2, 3, 4, ...
-    S[0][0] = 0;
+    S_prev[0] = 0;
     for (int num_floor = 1; num_floor <= n; ++num_floor) {
-        S[0][num_floor] = num_floor - 1;
+        S_prev[num_floor] = num_floor - 1;
     }
+    DUMP (S_prev);
+    auto* S_prev_ptr = &S_prev, *S_ptr = &S;
 
     for (int cur_k = 2; cur_k <= k; ++cur_k) {
-        S[cur_k - 1][0] = 0;
-        S[cur_k - 1][1] = 0;
+        auto& cur_arr = *S_ptr;
+        auto& prev_arr = *S_prev_ptr;
+
+        cur_arr[0] = 0;
+        cur_arr[1] = 0;
         for (int num_floor = 2; num_floor <= n; ++num_floor) {
             std::size_t min_s = -1;
             for (int i = 1; i <= num_floor; ++i) {
                 std::size_t s = 1 + std::max (
-                    S[cur_k - 1][num_floor - i], S[cur_k - 2][i]
+                    cur_arr[num_floor - i], prev_arr[i]
                 );
                 
                 if (s < min_s) {
@@ -35,11 +36,13 @@ solve (std::size_t n, std::size_t k) {
                 }
             }
             
-            S[cur_k - 1][num_floor] = min_s;
+            cur_arr[num_floor] = min_s;
         }
+        DUMP (S);
+        std::swap (S_ptr, S_prev_ptr);
     }
 
-    return S[k - 1][n];
+    return (*S_prev_ptr)[n];
 }
 
 int main () {
