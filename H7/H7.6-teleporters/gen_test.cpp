54487158691c554a4623de41fc67048571a0de52
DenRen <denrenruslan@gmail.com>
2022-04-19 20:52:29 +0300

    Попробовал нивными графами - не получилось

--- H7/H7.6-teleporters/gen_test.cpp
+++ H7/H7.6-teleporters/gen_test.cpp
@@ -7,7 +7,14 @@ gen_test (std::string in_name, std::string out_name) {
     std::fstream is {in_name,  std::ios_base::out};
     std::fstream os {out_name, std::ios_base::out};
 
+    int N = 2000;
+    is << N << '\n';
 
+    seclib::RandomGenerator rand;
+    for (int i = 0; i < N; ++i) {
+        auto str = rand.get_string (N, '0', '1');
+        is << str << '\n';
+    }
 }
 
 int main () {
--- H7/H7.6-teleporters/main.cpp
+++ H7/H7.6-teleporters/main.cpp
@@ -1,5 +1,57 @@
 #include "solve.hpp"
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void
+fill_str (const std::vector <std::vector <int>>& graph,
+          int begin_i,
+          std::string& str,
+          std::set <int>& set) {
+    if (set.find (begin_i) != set.cend ()) {
+        return;
+    }
+
+    set.insert (begin_i);
+
+    const auto& row = graph[begin_i];
+    for (int i : row) {
+        str[i] = '1';
+        fill_str (graph, i, str, set);
+    }
+}
+
 int main () {
     std::ios_base::sync_with_stdio (false);
+
+    int N = 0;
+    std::cin >> N;
+
+    std::vector <std::vector <int>> graph (N);
+
+    std::string str;
+    for (int i = 0; i < N; ++i) {
+        auto& row = graph[i];
+
+        std::cin >> str;
+        for (auto j = str.find ('1'); j != std::string::npos;
+                  j = str.find ('1', j + 1)) {
+            row.push_back (j);
+        }
+    }
+    
+    for (const auto& r : graph) {
+        DUMP (r);
+    }
+
+    str.resize (N);
+    std::set <int> set;
+    for (int i = 0; i < N; ++i) {
+        std::fill_n (str.begin (), N, '0');
+        set.clear ();
+
+        fill_str (graph, i, str, set);
+        std::cout << str << '\n';
+    }
 }
